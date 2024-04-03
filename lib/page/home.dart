19a03278a42638d9843ab01b0acf6ad389996806
AnandaRFebriyana <e41222236@student.polije.ac.id>
2024-04-03 09:21:27 +0700

    merge home

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -5,7 +5,6 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
