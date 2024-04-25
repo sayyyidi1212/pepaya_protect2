8c5cbe4bcea49cd70f0ea3600b1eb17b108dad53
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-04-25 08:03:24 +0700

    fix: home page

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -9,14 +10,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  DateTime today = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    DateTime today = DateTime.now();
     return Scaffold(
       body: Column(
         children: [
           Container(
-            padding: const EdgeInsets.only(top: 40, left: 15, right: 15, bottom: 10),
+            padding: const EdgeInsets.only(top: 40, left: 15, right: 15, bottom: 5),
             decoration: const BoxDecoration(
               color: Color(0xFF356899),
               borderRadius: BorderRadius.only(
@@ -32,7 +34,7 @@ class _HomeState extends State<Home> {
                     children: [
                       SizedBox(height: 20),
                       Padding(
-                        padding: const EdgeInsets.only(left: 3, bottom: 3),
+                        padding: EdgeInsets.only(left: 3, bottom: 3),
                         child: Text("Welcome Back!",
                           style: GoogleFonts.poppins(
                             fontSize: 14,
@@ -44,7 +46,7 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(left: 3, bottom: 25),
+                        padding: EdgeInsets.only(left: 3, bottom: 25),
                         child: Text("Fanidiya Tasya",
                           style: GoogleFonts.poppins(
                             fontSize: 20,
@@ -59,7 +61,7 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(left: 10),
+                  margin: const EdgeInsets.only(left: 10),
                   width: 50,
                   height: 50,
                   decoration: const BoxDecoration(
@@ -73,207 +75,220 @@ class _HomeState extends State<Home> {
               ],
             ),
           ),
-          Expanded(
-            child: ListView(
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Today",
-                            style: GoogleFonts.poppins(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          TextButton(
-                            child: Text("Apply for permission",
-                              style: GoogleFonts.poppins(
-                                color: Colors.red,
-                              ),
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
+                Text("Today",
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () => Get.toNamed('/presensi'),
-                      child: Container(
-                        width: 165,
-                        height: 134,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 5,
-                            ),
-                          ],
-                        ),
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              top: 20,
-                              left: 20,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      Container(
-                                        width: 40,
-                                        height: 40,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(10),
-                                          color: Color(0xFF3085FE).withOpacity(0.1),
-                                        ),
-                                        child: Icon(Icons.login),
-                                      ),
-                                      SizedBox(width: 5),
-                                      Text('Check In',
-                                        style: GoogleFonts.poppins(fontSize: 16),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text('06:00 AM',
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text('Go to Work',
-                                    style: GoogleFonts.poppins(fontSize: 15),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                TextButton(
+                  child: Text("Apply for permission",
+                    style: GoogleFonts.poppins(
+                      color: Colors.red,
                     ),
-                    GestureDetector(
-                      // onTap: () => Get.toNamed('/presensi'),
-                      child: Container(
-                        width: 165,
-                        height: 134,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 5,
-                            ),
-                          ],
-                        ),
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              top: 20,
-                              left: 20,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      Container(
-                                        width: 40,
-                                        height: 40,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(10),
-                                          color: Color(0xFF3085FE).withOpacity(0.1),
-                                        ),
-                                        child: Icon(Icons.exit_to_app),
-                                      ),
-                                      SizedBox(width: 5),
-                                      Text('Check Out',
-                                        style: GoogleFonts.poppins(fontSize: 16),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text('2:00 PM',
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text('Go Home',
-                                    style: GoogleFonts.poppins(fontSize: 15),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Stack(
-                      children: [
-                        TableCalendar(
-                          locale: "en_US",
-                          rowHeight: 40,
-                          headerStyle: const HeaderStyle(
-                            formatButtonVisible: false,
-                            titleCentered: true,
-                          ),
-                          focusedDay: today,
-                          firstDay: DateTime.utc(2012),
-                          lastDay: DateTime.utc(2040),
-                          calendarFormat: CalendarFormat.week,
-                          calendarStyle: const CalendarStyle(
-                            todayDecoration: BoxDecoration(
-                              color: Color(0xFF356899),
-                              shape: BoxShape.circle,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    // SizedBox (height: 15),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("History Activity",
-                            style: GoogleFonts.poppins(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          TextButton(
-                            child: Text("See all",
-                              style: GoogleFonts.poppins(
-                                color: Colors.blue,
-                              ),
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                    
-                  ],
+                  ),
+                  onPressed: () {},
                 ),
               ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () => Get.toNamed('/presensi'),
+                child: Container(
+                  width: 165,
+                  height: 134,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 20,
+                        left: 20,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color(0xFF3085FE).withOpacity(0.1),
+                                  ),
+                                  child: const Icon(FontAwesomeIcons.signIn),
+                                ),
+                                SizedBox(width: 5),
+                                Text("Check In",
+                                  style: GoogleFonts.poppins(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Text("06:00 AM",
+                              style: GoogleFonts.poppins(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text("Go to Work",
+                              style: GoogleFonts.poppins(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                // onTap: () => _checkOut(context),
+                child: Container(
+                  width: 165,
+                  height: 134,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 20,
+                        left: 20,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Color(0xFF3085FE).withOpacity(0.1),
+                                  ),
+                                  // child: Icon(FontAwesomeIcons.signOut),
+                                ),
+                                SizedBox(width: 5),
+                                Text("Check Out",
+                                  style: GoogleFonts.poppins(fontSize: 16),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Text("2:00 PM",
+                              style: GoogleFonts.poppins(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text("Go Home",
+                              style: GoogleFonts.poppins(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  TableCalendar(
+                    locale: "id_ID",
+                    rowHeight: 40,
+                    headerStyle: const HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                    ),
+                    focusedDay: today,
+                    firstDay: DateTime.utc(2012),
+                    lastDay: DateTime.utc(2040),
+                    calendarFormat: CalendarFormat.week,
+                    calendarStyle: const CalendarStyle(
+                      todayDecoration: BoxDecoration(
+                        color: Color(0xFF356899),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              // SizedBox (height: 15),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("History Activity",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    TextButton(
+                      child: Text("See all",
+                        style: GoogleFonts.poppins(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Item $index"),
+                        SizedBox(height: 5),
+                        Text("Details of item $index"),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
