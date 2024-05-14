8e43f7f8e1668da987f728587fe8b96305e65c35
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-05-14 13:57:39 +0700

    chore: reorganize folder structure

--- lib/routes/route_app.dart
+++ lib/routes/route_app.dart
@@ -1,29 +1,25 @@
 import 'package:get/get.dart';
-import 'package:patrol_track_mobile/forgot_password.dart';
-import 'package:patrol_track_mobile/login.dart';
-import 'package:patrol_track_mobile/menu_nav.dart';
-import 'package:patrol_track_mobile/otp.dart';
-import 'package:patrol_track_mobile/page/help_center.dart';
-import 'package:patrol_track_mobile/page/permission.dart';
-import 'package:patrol_track_mobile/page/presensi.dart';
-import 'package:patrol_track_mobile/page/scanner.dart';
-import 'package:patrol_track_mobile/reset_password.dart';
+import 'package:patrol_track_mobile/pages/auth/forgot_password.dart';
+import 'package:patrol_track_mobile/pages/auth/login.dart';
+import 'package:patrol_track_mobile/pages/auth/otp.dart';
+import 'package:patrol_track_mobile/pages/auth/reset_password.dart';
+import 'package:patrol_track_mobile/pages/menu_nav.dart';
+import 'package:patrol_track_mobile/pages/help_center.dart';
+import 'package:patrol_track_mobile/pages/permission.dart';
+import 'package:patrol_track_mobile/pages/presensi.dart';
+import 'package:patrol_track_mobile/pages/scanner.dart';
 
 class RouteApp {
   static final pages = [
-    GetPage(name: '/login', page: () => Login()),
-    GetPage(name: '/forgot-pass', page: () => ForgotPass()),
-    GetPage(name: '/otp', page: () => Otp()),
-    GetPage(name: '/reset-pass', page: () => ResetPassword()),
+    GetPage(name: '/login', page: () => const Login()),
+    GetPage(name: '/forgot-pass', page: () => const ForgotPass()),
+    GetPage(name: '/otp', page: () => const Otp()),
+    GetPage(name: '/reset-pass', page: () => const ResetPassword()),
     
     GetPage(name: '/menu-nav', page: () => MenuNav()),
     GetPage(name: '/presensi', page: () => Presensi()),
-<<<<<<< HEAD
+    GetPage(name: '/scanner', page: () => Scanner()),
     GetPage(name: '/help-center', page: () => HelpCenter()),
     GetPage(name: '/permission', page: () => Permission()),
-=======
-    GetPage(name: '/scanner', page: () => Scanner()),
-    GetPage(name: '/help-center', page: () => HelpCenter())
->>>>>>> 72cd12d67fd52b25e7db94cf6af4f99059cfaecd
   ];
 }
