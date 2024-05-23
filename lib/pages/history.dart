ccd52e14f4360b332e7970b9832cca4431dfa25a
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-05-23 08:56:05 +0700

    fix: fix display function for schedule

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -8,12 +8,11 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
-  
   Widget buildCard(String title, String description) {
     return ListTile(
       title: Text(
         title,
-        style: TextStyle(fontWeight: FontWeight.bold),
+        style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
       ),
       subtitle: Text(description),
     );
@@ -27,12 +26,17 @@ class _HistoryState extends State<History> {
           const Header(title: "History Activity"),
           Expanded(
             child: ListView.builder(
-              itemCount: 10, 
+              itemCount: 10,
               itemBuilder: (context, index) {
                 List<String> titles = ["Patrol 1", "Patrol 2", "Patrol 3"];
-                List<String> descriptions = ["Post 1 - Blok A", "Post 2 - Blok B", "Post 3 - Blok C"];
+                List<String> descriptions = [
+                  "Post 1 - Blok A",
+                  "Post 2 - Blok B",
+                  "Post 3 - Blok C"
+                ];
                 return Card(
-                  child: buildCard(titles[index % titles.length], descriptions[index % descriptions.length]),
+                  child: buildCard(titles[index % titles.length],
+                      descriptions[index % descriptions.length]),
                 );
               },
             ),
